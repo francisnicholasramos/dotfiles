f2b88cab3513c2af22b7f372944a61782baaf41a
francisnicholasramos <pfrancisnicholasramos@gmail.com>
2025-10-29 18:53:48 +0800

    Add ignorecase and tab spacing into 4

--- nvim/lua/set.lua
+++ nvim/lua/set.lua
@@ -5,9 +5,9 @@ vim.g.netrw_banner = 0;
 vim.opt.number = true 
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 2 
-vim.opt.softtabstop = 2 
-vim.opt.shiftwidth = 2 
+vim.opt.tabstop = 4 
+vim.opt.softtabstop = 4 
+vim.opt.shiftwidth = 4 
 vim.opt.expandtab = true 
 
 vim.opt.smartindent = true 
@@ -16,6 +16,7 @@ vim.opt.autoindent = true
 vim.opt.swapfile = false 
 vim.opt.backup = false 
 
+vim.opt.ignorecase = true
 vim.opt.hlsearch = true
 vim.opt.cursorline = true 
 
