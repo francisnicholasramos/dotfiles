4f2eb723889f1af385befca88ee89917b955b088
francisnicholasramos <pfrancisnicholasramos@gmail.com>
2025-11-01 22:42:37 +0800

    Update

--- nvim/lua/lazy_init.lua
+++ nvim/lua/lazy_init.lua
@@ -10,3 +10,9 @@ if not vim.loop.fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    spec = "lazy_plugins",
+    change_detection = { notify = false }
+})
+
