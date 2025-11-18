1136ca6717c03a558a5e18176fd2620a419fcf82
francisnicholasramos <pfrancisnicholasramos@gmail.com>
2025-11-18 13:30:23 +0800

    Update

--- nvim/lua/lazy_plugins/multicur.lua
+++ nvim/lua/lazy_plugins/multicur.lua
@@ -1,9 +1,6 @@
 return {
     {
         "mg979/vim-visual-multi",
-        branch = "master",
-        init = function() 
-            vim.g.VM_default_mappings = 0
-        end,
+        branch = "master"
     },
 }
