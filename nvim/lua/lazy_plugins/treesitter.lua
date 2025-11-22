f1e6b085261f13391b3660eb84a56a4254215d27
francisnicholasramos <pfrancisnicholasramos@gmail.com>
2025-11-23 02:09:50 +0800

    Add language parsers and syntax highlighting handling
    Added languages to ensure_installed and improved highlight disable logic.

--- nvim/lua/lazy_plugins/treesitter.lua
+++ nvim/lua/lazy_plugins/treesitter.lua
@@ -5,9 +5,20 @@ return {
         event = "BufRead",
         config = function()
             require("nvim-treesitter.configs").setup({
+                ensure_installed = {
+                    "typescript", "javascript", "bash", "html"
+                },
 
                 highlight = { 
-                    enable = false 
+                    enable = false,
+
+                    disable = function(lang, buf)
+                        local max_filesize = 20 * 1024 -- 100 KB
+                        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                        if ok and stats and stats.size > max_filesize then
+                            return true
+                        end
+                    end,
                 },
 
                 indent = { 
