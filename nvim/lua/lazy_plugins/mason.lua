fcd89bd9db36d5649274cb7d18f3949061bd7692
francisnicholasramos <pfrancisnicholasramos@gmail.com>
2025-12-02 11:04:35 +0800

    Add lazy load for mason

--- nvim/lua/lazy_plugins/mason.lua
+++ nvim/lua/lazy_plugins/mason.lua
@@ -1,37 +1,40 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        dependencies = { "williamboman/mason-lspconfig.nvim" },
-        config = function()
-            require("mason").setup()
-            require("mason-lspconfig").setup({
-                ensured_installed = {
-                    "html",
-                    "cssls",
-                    "ts_ls",
-                    "jsonls",
-                    "emmet-ls",
-                    "tailwindcss",
+    "williamboman/mason.nvim",
+    dependencies = { 
+        {
+            "williamboman/mason-lspconfig.nvim",
+            config = function()
+                require("mason-lspconfig").setup({
+                    ensure_installed = {
+                        "html",
+                        "cssls",
+                        "ts_ls",
+                        "eslint",
+                        "tailwindcss",
 
-                     -- "clangd",
-                     -- "cssls",
-                     -- "emmet_ls",
-                     -- "eslint",
-                     -- "html",
-                     -- "jdtls",
-                     -- "jsonls",
-                     -- "pyright",
-                     -- "tailwindcss",
-                     -- "ts_ls",
+                        -- "clangd",
+                        -- "cssls",
+                        -- "emmet_ls",
+                        -- "eslint",
+                        -- "html",
+                        -- "jdtls",
+                        -- "jsonls",
+                        -- "pyright",
+                        -- "tailwindcss",
+                        -- "ts_ls",
 
-                    
 
-                    -- ** Manually install **
-                    -- MasonInstall prettier@2.8.8
-                },
-                automatic_enable = false,
-            })      
 
-        end,			
+                        -- ** Manually install **
+                        -- MasonInstall prettier@2.8.8
+                    },
+                    automatic_enable = false,
+                })      
+            end,			
+        }
     },
+    cmd = {"Mason"},
+    config = function()
+        require("mason").setup()
+    end
 }
