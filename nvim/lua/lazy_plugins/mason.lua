d83e292f23cbe7db5b8aa8d7cd1a493f386c0cce
francisnicholasramos <pfrancisnicholasramos@gmail.com>
2026-03-25 18:25:27 +0800

    remove unused dependencies
    Removed dependencies and configuration for mason-lspconfig.

--- nvim/lua/lazy_plugins/mason.lua
+++ nvim/lua/lazy_plugins/mason.lua
@@ -1,45 +1,5 @@
 return {
     "williamboman/mason.nvim",
-    dependencies = {
-        {
-            "williamboman/mason-lspconfig.nvim",
-            config = function()
-                require("mason-lspconfig").setup({
-                    ensure_installed = {
-                        "html",
-                        "cssls",
-                        "ts_ls",
-                        "eslint",
-                        "tailwindcss",
-                        "phpactor",
-                        "intelephense",
-
-                        -- "html",
-                        -- "cssls",
-                        -- "ts_ls",
-                        -- "eslint",
-                        -- "tailwindcss",
-                        -- "phpactor",
-                        -- "intelephense",
-                        -- "blade-formatter",
-                        -- "pyright",
-                        -- "laravel_ls",
-                        -- "lua_ls"
-                        -- "clangd",
-                        -- "jdtls",
-                        -- "jsonls",
-                        -- "emmet_ls",
-
-
-
-                        -- ** Manually install **
-                        -- MasonInstall prettier@2.8.8
-                    },
-                    automatic_enable = false,
-                })
-            end,
-        }
-    },
     cmd = {"Mason"},
     config = function()
         require("mason").setup()
